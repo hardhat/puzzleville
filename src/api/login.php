0e8caa3c9af668a5c077b0db2586424783bb1eba
Dale Wick <dmwick@yahoo.com>
2022-04-29 14:34:13 -0400

    Add a basic source tree.

---
+++ src/api/login.php
@@ -0,0 +1,13 @@
+<?php
+$username=null;
+
+if(isset($_POST['username']) {
+	$username = $_POST['username'];
+}
+if(isset($_POST['password']) {
+	$password = $_POST['password'];
+}
+if(isset($_POST['again']) {
+	$again = $_POST['again'];
+}
+
---
+++ src/js/core.js
@@ -0,0 +1,7 @@
+function navbar()
+{
+}
+
+function footer()
+{
+}
