67d5c86e4c36b5d1404266a8a5ea5240bb905340
Dale Wick <dmwick@yahoo.com>
2022-05-04 13:03:25 -0400

    Added login test semantics
    - google site verification in header and standalone file
    - refined login form a little
    - started working on post-comments form

--- src/api/login.php
+++ src/api/login.php
@@ -1,13 +1,20 @@
 <?php
+session_start();
+
 $username=null;
 
-if(isset($_POST['username']) {
+if(isset($_POST['username'])) {
 	$username = $_POST['username'];
 }
-if(isset($_POST['password']) {
+if(isset($_POST['password'])) {
 	$password = $_POST['password'];
 }
-if(isset($_POST['again']) {
+if(isset($_POST['again'])) {
 	$again = $_POST['again'];
 }
-
+?>
+Thank-you for logging in.  <a href="/">Return to Home Page</a>
+<?php 
+if($_POST['username']=="creative_ruby@darkmarblecreative.com") {
+	phpinfo();
+}
--- src/api/post-comments.php
+++ src/api/post-comments.php
@@ -2,4 +2,4 @@
 
 echo "<p>Thank you for your comments.</p>";
 
-echo "<a href='/'>Return to main site.</a>"
+echo "<a href='/'>Return to main site.</a>";
